69be9b945ad362044d9efb1e96b9eeffcabdcd2f
Faza Zikri Ramadhan <faza.ramadhan@zetta-intern.space>
2026-08-07 14:19:21 +0700

    [LEARNING_MATERIAL_006] Fix - fix typo from .$regex to .regex on grading.validator

--- src/features/academic/grading/grading.validator.js
+++ src/features/academic/grading/grading.validator.js
@@ -14,7 +14,7 @@ const SubmitTestGradesSchema = Joi.object({
   grades: Joi.array()
     .items(
       Joi.object({
-        student_id: Joi.string().required().$regex(OBJECT_ID_PATTERN),
+        student_id: Joi.string().required().regex(OBJECT_ID_PATTERN),
         score: Joi.number().min(0).max(100).required(),
       }),
     )
